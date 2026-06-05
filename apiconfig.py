90b680cce33c107682061a60c60ae6de7ea37c7c
frfaizanahmed <frfaizanahmed@gmail..com>
2026-06-05 10:26:40 +0530

    Initial commit

---
+++ apiconfig.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import streamlit as st
+
+api_key = st.secrets["GROQ_API_KEY_1"]
+api_key2 = st.secrets["GROQ_API_KEY_2"]
+
+# client = OpenAI(
+#     api_key=api_key,
+#     base_url="https://api.groq.com/openai/v1"
+# )
+
+def ai_help(prompt):
+    try:
+        client = OpenAI(
+        api_key=api_key,
+        base_url="https://api.groq.com/openai/v1")
+        response = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+        result = response.choices[0].message.content
+        return result
+
+    except Exception as e:
+        try:
+            client = OpenAI(
+            api_key=api_key2,
+            base_url="https://api.groq.com/openai/v1")
+            response = client.chat.completions.create(
+                model="llama-3.3-70b-versatile",
+                messages=[
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            result = response.choices[0].message.content
+            return result
+        except Exception as e2:
+            return f"API 1 & 2 both Failed, Error1: {e} Error2: {e2}"
