7e782eb4fea410567ef041f5a022a7dda1e69889
frfaizanahmed <frfaizanahmed@gmail..com>
2026-06-07 10:51:36 +0530

    Correct version name

--- components/footer.py
+++ components/footer.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def show_footer():
     st.caption(
-            "© 2026 Faizan Ahmed Makki | AI-Powered Data Analytics Platform | All Rights Reserved."
-        )
+    "© 2026 Faizan Ahmed Makki | AI-Powered Data Analytics Platform | Version 2.0 | All Rights Reserved."
+    )
